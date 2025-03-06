e3f29ddb846ba516c7f7057f0bc3126636eb87bd
Iulian <54375582+poenaruiulian@users.noreply.github.com>
2025-03-06 18:19:59 +0200

    Hooks memory optimization (#90)

--- src/hooks/useStep.ts
+++ src/hooks/useStep.ts
@@ -5,6 +5,7 @@ import {
   StepModel,
 } from '@constants';
 import { useMutation } from '@apollo/client';
+import { useCallback } from 'react';
 import { useError } from './useError';
 
 export const useStep = () => {
@@ -13,31 +14,31 @@ export const useStep = () => {
   const [breakStepMutation] = useMutation(BRAKE_STEP);
   const [changeStepStateMutation] = useMutation(CHANGE_STEP_STATE);
 
-  const changeStepState = async ({
-    courseId,
-    stepId,
-  }: {
-    courseId: CourseModel['id'];
-    stepId: StepModel['id'];
-  }) =>
-    handleError(
-      await changeStepStateMutation({
-        variables: { courseId, stepId },
-      }).catch(err => err)
-    );
+  const changeStepState = useCallback(
+    async ({
+      courseId,
+      stepId,
+    }: {
+      courseId: CourseModel['id'];
+      stepId: StepModel['id'];
+    }) =>
+      handleError(
+        await changeStepStateMutation({
+          variables: { courseId, stepId },
+        }).catch(err => err)
+      ),
+    [changeStepStateMutation, handleError]
+  );
 
-  const breakStep = async ({
-    stepId,
-    feedback,
-  }: {
-    stepId: number;
-    feedback: string;
-  }) =>
-    handleError(
-      await breakStepMutation({
-        variables: { stepId, feedback },
-      }).catch(err => err)
-    );
+  const breakStep = useCallback(
+    async ({ stepId, feedback }: { stepId: number; feedback: string }) =>
+      handleError(
+        await breakStepMutation({
+          variables: { stepId, feedback },
+        }).catch(err => err)
+      ),
+    [breakStepMutation, handleError]
+  );
 
   return {
     changeStepState,
